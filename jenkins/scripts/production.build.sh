dff86cefc6c935a73dd772d47ac4a3f39f89b757
bruce grover lee <brucegrover.lee@gmail.com>
2019-07-11 11:50:06 -0500

    add: copy postcss.config.js file

--- jenkins/scripts/production.build.sh
+++ jenkins/scripts/production.build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-webpack --mode production \
+echo "copy postcss.config.js file into the node_modules":
+cp "./src/styles/postcss.config.js" "./node_modules/grapesjs/dist/css/postcss.config.js"
+
+echo "Start build for production";
+
+./node_modules/.bin/webpack --mode production \
   --env.ENVIRONMENT=production \
   --env.ENV_FILE_PATH=$ADMIN_PRODUCTION_ENV \
   --env.FIREBASE_CONFIG_PATH=$ADMIN_PRODUCTION_FIREBASE_CONFIG_JSON \
--- jenkins/scripts/staging.build.sh
+++ jenkins/scripts/staging.build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-webpack --mode production \
+echo "copy postcss.config.js file into the node_modules":
+cp "./src/styles/postcss.config.js" "./node_modules/grapesjs/dist/css/postcss.config.js"
+
+echo "Start build for staging";
+
+./node_modules/.bin/webpack --mode production \
   --env.ENVIRONMENT=staging \
   --env.ENV_FILE_PATH=$ADMIN_STAGING_ENV \
   --env.FIREBASE_CONFIG_PATH=$ADMIN_STAGING_FIREBASE_CONFIG_JSON \
