cdc563237d7a73808467d27172ea1998544e6b3f
bruce grover lee <brucegrover.lee@gmail.com>
2019-07-11 12:29:30 -0500

    fix: path of firebase

--- jenkins/scripts/production.deploy.sh
+++ jenkins/scripts/production.deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-firebase deploy --only hosting:admin -P production --token $FIREBASE_DEPLOY_TOKEN
+# firebase deploy --only hosting:admin -P production --token $FIREBASE_DEPLOY_TOKEN
+
+./home/node/.npm-global/lib/node_modules/firebase-tools/lib/bin/firebase.js \
+  deploy --only hosting:admin -P production --token $FIREBASE_DEPLOY_TOKEN
